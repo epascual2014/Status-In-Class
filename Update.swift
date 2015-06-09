388c3f34d69a11be9e3e75ed19537d7d5939b079
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2015-06-08 19:12:56 -0700

    Add Models

---
+++ Update.swift
@@ -0,0 +1,18 @@
+//
+//  Update.swift
+//  Status In Class
+//
+//  Created by Edrick Pascual on 6/8/15.
+//  Copyright (c) 2015 Edrick Pascual. All rights reserved.
+//
+
+import Foundation
+
+class Update {
+    
+    var user: User?
+    var text: String?
+    var date: NSDate?
+    
+    
+}
---
+++ User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Status In Class
+//
+//  Created by Edrick Pascual on 6/8/15.
+//  Copyright (c) 2015 Edrick Pascual. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    // example of twitter attributes
+    var name: String?
+    var bio: String?
+    var userName: String?
+    var link: String?
+    var city: String?
+    
+    
+    
+}
